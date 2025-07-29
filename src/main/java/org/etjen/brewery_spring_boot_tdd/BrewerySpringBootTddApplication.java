e564bd783051cb6accd97d72447fea88856503f5
etjen <buljubasicetjen4@gmail.com>
2025-07-29 16:25:48 +0200

    Initial commit

---
+++ src/main/java/org/etjen/brewery_spring_boot_tdd/BrewerySpringBootTddApplication.java
@@ -0,0 +1,13 @@
+package org.etjen.brewery_spring_boot_tdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrewerySpringBootTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrewerySpringBootTddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/etjen/brewery_spring_boot_tdd/BrewerySpringBootTddApplicationTests.java
@@ -0,0 +1,13 @@
+package org.etjen.brewery_spring_boot_tdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrewerySpringBootTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
